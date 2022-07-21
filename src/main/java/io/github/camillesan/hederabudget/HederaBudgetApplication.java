272c273329e38974d19136eabc8edce476072934
Camille <camille.leonard@belighted.com>
2022-07-21 19:50:36 +0200

    init project

---
+++ src/main/java/io/github/camillesan/hederabudget/HederaBudgetApplication.java
@@ -0,0 +1,13 @@
+package io.github.camillesan.hederabudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HederaBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HederaBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/camillesan/hederabudget/HederaBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.camillesan.hederabudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HederaBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
